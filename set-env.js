c3a315739580d1a12cf8536513a229318d6fe4bc
Michaela Jane Faustino <faustino.michajane@gmail.com>
2025-09-15 22:56:02 +0800

    set proper structure of environment

--- set-env.js
+++ set-env.js
@@ -4,10 +4,10 @@ const targetPath = './src/environments/environment.prod.ts';
 const envConfigFile = `
 export const environment = {
   production: true,
-  emailJsServiceId: process.env?.['EMAILJS_SERVICE_ID'],
-  emailJsTemplateId: process.env?.['EMAILJS_TEMPLATE_ID'],
-  emailJsPublicKey: process.env?.['EMAILJS_PUBLIC_KEY'],
-  baseUrl: process.env?.['BASE_URL'],
+  emailJsServiceId: ${process.env?.['EMAILJS_SERVICE_ID']},
+  emailJsTemplateId: ${process.env?.['EMAILJS_TEMPLATE_ID']},
+  emailJsPublicKey: ${process.env?.['EMAILJS_PUBLIC_KEY']},
+  baseUrl: ${process.env?.['BASE_URL']},
 };
 `;
 writeFileSync(targetPath, envConfigFile);
