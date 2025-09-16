864b50bccf75b53828a2c6ae8a6ec7e972b9a345
Michaela Jane Faustino <faustino.michajane@gmail.com>
2025-09-16 13:50:20 +0800

    set prod to true

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 import { Environment } from "./environment.model";
 
 export const environment: Environment = {
-  production: false,
+  production: true,
   emailJsServiceId: '',
   emailJsTemplateId: '',
   emailJsPublicKey: '',
-  baseUrl: ''
+  baseUrl: 'https://portfolio-api-v1-dcji.onrender.com'
 };
